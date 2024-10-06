8dc90bfc1d47373e721d4357438d1be03ccb480f
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-10-06 07:53:51 -0600

    fix: :bug: change Header -> header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import { Toaster } from "@/components/ui/toaster"
 
 
