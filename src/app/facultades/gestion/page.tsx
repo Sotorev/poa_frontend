70af4d49ad73919fd72d74307eb2e540ae42c4a7
Keithleen Ivana Ruíz Estrada <125908174+keithre19@users.noreply.github.com>
2024-10-10 21:33:50 -0600

    feat: :sparkles: add faculty management page

---
+++ src/app/facultades/gestion/page.tsx
@@ -0,0 +1,18 @@
+import { getServerSession } from '@/lib/server-auth'
+import { hasPermission } from '@/lib/auth'
+import { redirect } from 'next/navigation'
+import FacultyComponent from '@/components/faculty/faculties-component'
+
+export default async function UsersPage() {
+  const session = await getServerSession()
+
+  if (!session) {
+    redirect('/iniciar-sesion')
+  }
+
+  if (!hasPermission(session, 'ODS', 'Edit')) {
+    redirect('/no-authorizado')
+  }
+
+  return <FacultyComponent />
+}
