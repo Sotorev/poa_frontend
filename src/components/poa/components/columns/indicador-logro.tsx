cd54eee2183c8ebf54027ae25eb26074478dd97c
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-10-14 17:09:30 -0600

    Refactor IndicadorLogroComponent to add styling and placeholder

--- src/components/poa/components/columns/indicador-logro.tsx
+++ src/components/poa/components/columns/indicador-logro.tsx
@@ -1,4 +1,3 @@
-// src/components/poa/components/columns/indicador-logro.tsx
 'use client';
 
 import React from 'react';
@@ -14,6 +13,8 @@ export function IndicadorLogroComponent({ value, onChange }: IndicadorLogroCompo
     <Input
       value={value}
       onChange={(e) => onChange(e.target.value)}
+      className="w-64 border-green-300 focus:border-green-500 focus:ring-green-500"
+      placeholder="Indicador de logro"
     />
   );
 }
