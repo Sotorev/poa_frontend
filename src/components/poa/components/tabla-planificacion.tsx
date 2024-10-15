cfc5339fe3f91c1eb577c6f5fd8dbb13190e4dd1
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-10-15 09:50:00 -0600

    hotfix types estado

--- src/components/poa/components/tabla-planificacion.tsx
+++ src/components/poa/components/tabla-planificacion.tsx
@@ -50,7 +50,7 @@ const initialOptions: PurchaseType[] = [
 type FilaPlanificacionForm = z.infer<typeof filaPlanificacionSchema>;
 
 interface FilaPlanificacion extends FilaPlanificacionForm {
-  estado: 'planificado' | 'aprobado' | 'rechazado';
+  estado: 'En revisión' | 'Aprobado' | 'Rechazado';
   comentarioDecano: string;
 }
 
@@ -160,7 +160,7 @@ export function TablaPlanificacionComponent() {
       tipoEvento: 'actividad',
       evento: '',
       objetivo: '',
-      estado: 'planificado',
+      estado: 'En revisión',
       costoTotal: 0,
       aporteUMES: [],
       aporteOtros: [],
@@ -342,7 +342,7 @@ export function TablaPlanificacionComponent() {
       // Opcional: Actualizar el estado de la fila, por ejemplo, cambiar el estado a 'aprobado'
       setFilas(prevFilas =>
         prevFilas.map(filaItem =>
-          filaItem.id === id ? { ...filaItem, estado: 'aprobado' } : filaItem
+          filaItem.id === id ? { ...filaItem, estado: 'En revisión' } : filaItem
         )
       );
 
