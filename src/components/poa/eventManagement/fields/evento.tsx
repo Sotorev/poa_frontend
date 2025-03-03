3c8d68bdcf2ce93c3b89cb926af5295fd085b479
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2025-03-03 17:29:38 -0600

    refactor: :recycle: rename EventoComponent and ObjetivoComponent for clarity and integrate react hook form

--- src/components/poa/eventManagement/fields/evento.tsx
+++ src/components/poa/eventManagement/fields/evento.tsx
@@ -4,18 +4,18 @@
 import React from 'react';
 import { Input } from '@/components/ui/input';
 
-interface EventoComponentProps {
+interface EventNameComponentProps {
   value: string;
   onChange: (value: string) => void;
 }
 
-export function EventoComponent({ value, onChange }: EventoComponentProps) {
+export function EventNameComponent({ value, onChange }: EventNameComponentProps) {
   return (
     <Input
       value={value}
       onChange={(e) => onChange(e.target.value)}
       className="w-64 border-green-300 focus:border-green-500 focus:ring-green-500 text-green-700 placeholder-green-400"
-      placeholder="Ingrese el evento"
+      placeholder="Ingrese el nombre del evento"
     />
   );
 }
--- src/components/poa/eventManagement/fields/objetivo.tsx
+++ src/components/poa/eventManagement/fields/objetivo.tsx
@@ -4,18 +4,18 @@
 import React from 'react';
 import { Textarea } from '@/components/ui/textarea';
 
-interface ObjetivoComponentProps {
+interface ObjectiveComponentProps {
   value: string;
   onChange: (value: string) => void;
 }
 
-export function ObjetivoComponent({ value, onChange }: ObjetivoComponentProps) {
+export function ObjectiveComponent({ value, onChange }: ObjectiveComponentProps) {
   return (
     <Textarea
       value={value}
       onChange={(e) => onChange(e.target.value)}
       className="w-64 border-green-300 focus:border-green-500 focus:ring-green-500 text-green-700 placeholder-green-400"
-      placeholder="Ingrese el objetivo"
+      placeholder="Ingrese el objetivo del evento"
     />
   );
 }
