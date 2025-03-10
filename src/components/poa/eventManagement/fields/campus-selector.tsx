cfa87795454adca5c5b3028c6c2ef67761b20cff
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2025-03-10 00:37:54 -0600

    refactor: :recycle: integrate EventContext into CampusSelector for campus data management

--- src/components/poa/eventManagement/fields/campus-selector.tsx
+++ src/components/poa/eventManagement/fields/campus-selector.tsx
@@ -1,10 +1,9 @@
 // src/components/poa/components/columns/campus-selector.tsx
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { useCurrentUser } from '@/hooks/use-current-user';
-import { getCampuses } from '@/services/apiService';
 import { Campus } from '@/types/Campus';
+import { EventContext } from '../formView/event.context';
 
 interface CampusSelectorProps {
   onSelectCampus: (campusId: number) => void;
@@ -12,34 +11,11 @@ interface CampusSelectorProps {
 }
 
 export function CampusSelector({ onSelectCampus, selectedCampusId }: CampusSelectorProps) {
-  const [campuses, setCampuses] = useState<Campus[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-  const user = useCurrentUser();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const data = await getCampuses(user?.token || '');
-        const activeCampuses = data.filter((campus) => !campus.isDeleted);
-        setCampuses(activeCampuses);
-      } catch (err) {
-        setError('No se pudieron cargar los campus.');
-        console.error(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [user?.token]);
-
-  if (loading) return <div>Cargando campus...</div>;
-  if (error) return <div className="text-red-500">Error: {error}</div>;
+  const { campuses } = useContext(EventContext)
 
   return (
-    <Select onValueChange={(value)=> onSelectCampus(Number.parseInt(value))} value={selectedCampusId?.toString()}>
+    <Select onValueChange={(value) => onSelectCampus(Number.parseInt(value))} value={selectedCampusId?.toString()}>
       <SelectTrigger className=" border-green-300 focus:ring-green-500 focus:border-green-500">
         <SelectValue placeholder="Selecciona un campus" />
       </SelectTrigger>
