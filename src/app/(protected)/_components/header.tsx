8aecf9373a41bf6fc5d0c4ded39d7f66fc9ddaab
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2025-04-03 09:17:43 -0600

    fix: :bug: inhabilitar opción "Finalizar" en el menú de navegación del encabezado
    - Se ha comentado la opción "Finalizar" en el componente de encabezado para evitar su visualización temporalmente para evitar que se visualicen los eventos de otras facus.

--- src/app/(protected)/_components/header.tsx
+++ src/app/(protected)/_components/header.tsx
@@ -106,7 +106,7 @@ const navItems: NavItem[] = [
 		requiredRoles: ['Decano', 'Administrador', 'Directora', 'Directora académica', 'Coordinador Pedagógico', 'Formulador'],
 		subItems: [
 			{ title: "Ejecutar", href: "/poa/ejecucion", description: "Eventos ejecutados", requiredRoles: ['Decano', 'Administrador', 'Directora', 'Directora académica', 'Coordinador Pedagógico', 'Formulador'], icon: FileText },
-			{ title: "Finalizar", href: "/poa/finalizar", description: "Finalizar el POA", requiredRoles: ['Decano', 'Administrador', 'Directora', 'Directora académica', 'Coordinador Pedagógico', 'Formulador'], icon: FileText },
+			// { title: "Finalizar", href: "/poa/finalizar", description: "Finalizar el POA", requiredRoles: ['Decano', 'Administrador', 'Directora', 'Directora académica', 'Coordinador Pedagógico', 'Formulador'], icon: FileText },
 		],
 	},
 	{
