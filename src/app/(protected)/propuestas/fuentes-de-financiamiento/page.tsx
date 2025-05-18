7a71449a4fc510b04cfeec0df18ed043d50c649a
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2025-05-18 15:48:52 -0600

    feat: :sparkles: Add new proposal sections for Campus, Financing Source, Purchase Type, and Resources with corresponding components

---
+++ src/app/(protected)/propuestas/fuentes-de-financiamiento/page.tsx
@@ -0,0 +1,7 @@
+import { FinancingSourceApprove } from "@/components/approveProposals/financingSource/UI.financingSourceApprove"
+
+export default function PropuestasFinancingSource() {
+    return (
+        <FinancingSourceApprove />
+    )
+}
---
+++ src/app/(protected)/propuestas/recursos/page.tsx
@@ -0,0 +1,7 @@
+import { ResourcesApprove } from "@/components/approveProposals/resources/UI.resourcesApprove"
+
+export default function PropuestasResources() {
+    return (
+        <ResourcesApprove />
+    )
+}
